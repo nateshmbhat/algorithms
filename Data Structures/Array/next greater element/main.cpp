b8aa6dcb238bd4c74a97388387d0258ba6273cc9
nateshmbhat <nateshmbhat1@gmail.com>
2018-01-22 13:15:56 +0530

    added next greatest ele

---
+++ Data Structures/Array/next greater element/main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<list>
+
+
+using namespace std ; 
+
+int main(void)
+{
+    
+    int a[] ={4,6,34,45,54,2,56,54,44,88 , 34 ,38 , 5 , 3 , 8} ; 
+    int n = sizeof(a)/sizeof(a[0]) ; 
+    int out[100] ; 
+
+    int max_yet = a[n-1] ; 
+
+}
