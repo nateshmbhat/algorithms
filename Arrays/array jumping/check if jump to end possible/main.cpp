399381de096aecf109582d70cf14cb56adfa9715
nateshmbhat <nateshmbhat1@gmail.com>
2018-06-02 09:42:11 +0530

    merge commit

--- Arrays/array jumping/check if jump to end possible/main.cpp
+++ Arrays/array jumping/check if jump to end possible/main.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 bool checkingFunction(vector<int> A)
 {
-    int furthest = A[0]-1 ; 
-    for(int i =1 ; i<A.size() ; i++ , furthest--)
+    int furthest = A[0]  , i ; 
+
+    for(i =0 ; i<A.size() ; i++ , furthest--)
     {
-        if(max(furthest , A[i])==0) return false ; 
+        if(max(furthest , A[i])<=0) return false ; 
         furthest = max(furthest , A[i]) ;
     }
-    return true ;
+    if(i==A.size()) return true ;
 }
 
 
@@ -19,8 +20,9 @@ int main(void)
 {
     vector<int> A = { 3,3,1,0 ,2,0,1} ; 
     vector<int> B = {3,2 , 0,0,2,0 , 1} ; 
-
+    vector<int> C = {0 , 1} ; 
     cout<< checkingFunction(A)<<endl; 
-    cout<<checkingFunction(B) ; 
+    cout<<checkingFunction(B) <<endl; ; 
+    cout<<checkingFunction(C ) ; 
 
 }
