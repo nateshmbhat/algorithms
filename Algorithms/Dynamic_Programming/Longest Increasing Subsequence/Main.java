60c423ea9fbee4643791d100251d60586ca3e742
nateshmbhat <nateshmbhat1@gmail.com>
2018-09-27 13:32:08 +0530

    update

---
+++ Algorithms/Dynamic_Programming/Longest Increasing Subsequence/Main.java
@@ -0,0 +1,56 @@
+import java.util.* ; 
+
+class Main{
+    static int nonDp = 0 ; 
+    static int Dp = 0 ; 
+    public static void main(String[] args) {
+        int[] arr = {10, 22, 9, 33, 21, 50, 41, 60, 80} ; 
+        // int[] arr ={3, 10, 2, 1, 20}  ; 
+        // int[] arr ={50, 3, 10, 7, 40, 80} ; 
+        LIS(arr) ; 
+    }
+
+    public static void LIS(int [] arr){
+        System.out.println( LISHelperNonDp(arr, 0 , Integer.MIN_VALUE )+" calls = " + nonDp) ;
+        HashMap<String,Integer> mem = new HashMap<>() ; 
+        System.out.println( LISHelperDp(arr, 0 , Integer.MIN_VALUE , mem )+ " calls = " + Dp ) ;
+    }
+
+    public static int LISHelperNonDp(int[] arr , int i , int cur){
+        nonDp+=1 ; 
+        if(i>=arr.length)return 0 ; 
+        if(arr[i]>cur) return Math.max(1+LISHelperNonDp(arr, i+1, arr[i]), LISHelperNonDp(arr, i+1, cur)) ; 
+        else return LISHelperNonDp(arr, i+1, cur) ;
+    }
+
+
+    public static int LISHelperDp(int[] arr , int i , int cur , HashMap<String,Integer> mem){
+        Dp+=1 ;
+        if(mem.containsKey(""+i+'-'+cur)) return mem.get(""+i+'-'+cur) ; 
+        if(i>=arr.length)return 0 ; 
+        int res1=Integer.MIN_VALUE , res2 ; 
+        if(arr[i]>cur) {
+            res1 = 1+LISHelperDp(arr, i+1, arr[i] , mem ) ; 
+            res2 =  LISHelperDp(arr, i+1, cur , mem) ; 
+        }
+        else { 
+            res2 = LISHelperDp(arr, i+1 , cur , mem) ;
+        }
+        mem.put(""+i+"-"+cur, Math.max(res1,res2)) ; 
+        return Math.max(res1,res2) ; 
+    }
+
+    public static int LISHelperBottomUpDp(int[] arr){
+        ArrayList<Integer> lis = new ArrayList<>() ; 
+        for(int i =0 ;i < arr.length ; i++)lis.add(1) ; 
+        
+        for(int i =1 ;i < arr.length ; i++){
+            for(int j=0 ; j<i ; j++ )
+            {
+                if(arr[i]>arr[j] && lis[i]>list[j]){
+                    
+                }
+            }
+        }
+    }
+}
